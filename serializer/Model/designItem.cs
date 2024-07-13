eedb65d516d3dae7ecc4d22423ae15ff46efba3c
yuvarajsf <yuvaraj.anbazhagan@syncfusion.com>
2024-07-13 11:10:12 +0530

    fix drag drop issue

--- serializer/Model/designItem.cs
+++ serializer/Model/designItem.cs
@@ -3,7 +3,13 @@ namespace serializer.Model;
 public class designItem
 {
     public string item { get; set; }
-    public string position { get; set; }
+    public WidgetPosition position { get; set; }
     public string name { get; set; }
     public string uniqueId { get; set; }
+}
+
+public class WidgetPosition
+{
+    public string x { get; set; }
+    public string y { get; set; }
 }
